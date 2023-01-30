51489f4bd3d501baf1d71d510148d1d4cee79f86
Samet Karakurt <sametkarakurt@Samet-Air.home>
2023-01-30 14:05:47 +0300

    Android Google Sign In Added

---
+++ App.js
@@ -0,0 +1,45 @@
+import {View, Text, TouchableOpacity} from 'react-native';
+import React from 'react';
+import {
+  GoogleSignin,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+
+GoogleSignin.configure({
+  webClientId:
+    '991915358860-umq4f0i5f9qkjdhbh5n8k7cl78f57k84.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+});
+
+// Somewhere in your code
+const signIn = async () => {
+  try {
+    await GoogleSignin.hasPlayServices();
+    const userInfo = await GoogleSignin.signIn();
+
+    alert(JSON.stringify(userInfo));
+    await GoogleSignin.signOut();
+  } catch (error) {
+    console.log(error);
+    if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+      // user cancelled the login flow
+    } else if (error.code === statusCodes.IN_PROGRESS) {
+      // operation (e.g. sign in) is in progress already
+    } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+      // play services not available or outdated
+    } else {
+      // some other error happened
+    }
+  }
+};
+
+const App = () => {
+  return (
+    <View>
+      <TouchableOpacity onPress={signIn}>
+        <Text>Google Sign ın</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default App;
